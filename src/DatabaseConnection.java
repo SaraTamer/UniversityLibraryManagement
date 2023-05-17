f7e0139d2908f466c46f54fb619b0198a6580476
SaraTamer <sasooelbihery@gmail.com>
2023-05-17 16:23:51 +0300

    In the name of God.

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:sqlserver://192.168.1.47:1433;database=UniversityLibraryManagement;encrypt=true;trustservercertificate=true";
+    private static final String UserName = "LibraryAdmin";
+    private static final String password = "13579";
+    private static Connection connection = null;
+    public static Connection getConnection()
+    {
+        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(URL, UserName, password);
+            if(connection == null)
+            {
+                System.out.println("the connection is null");
+            }
+            else{
+                System.out.println("the connection is not null");
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("com.Models.DBConnection.getConnection()" + e.getMessage());
+        }
+        return connection;
+    }
+}
