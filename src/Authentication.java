38dd8ef61d2d4afbdb537bc923bfc81bb6857431
SaraTamer <sasooelbihery@gmail.com>
2023-05-18 13:36:51 +0300

    initialize classes

--- src/Authentication.java
+++ src/Authentication.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class Insert {
+public class Authentication {
     Connection connection = null;
     PreparedStatement ps = null;
     CallableStatement callableStatement = null;
