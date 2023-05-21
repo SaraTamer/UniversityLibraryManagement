922f746a450a3e86f44eee22be6daaf3d43146a2
ayaaalli <ayaali95248@gmail.com>
2023-05-22 01:14:40 +0300

    Sign up and log in gui

--- src/Display.java
+++ src/Display.java
@@ -5,14 +5,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Display {
-    public void viewAllBooks() throws SQLException {
-        String query = "SELECT * FROM book as b, author as a WHERE b.ISBN = a.ISBN";
+    public void viewAllBooks() {
+        String query = "SELECT * FROM book AS b, author AS a WHERE b.ISBN = a.ISBN";
         ResultSet resultSet = null;
         Connection connection = null;
         PreparedStatement ps = null;
         ArrayList<String> bookList = new ArrayList<>();
-        bookList.add(String.format("%-10s | %-20s | %-20s | %-10s | %-7s | %-20s | %s" ,
-                "ISBN", "Title", "Category", "Language", "Edition", "Publishing Year" , "Author" ));
+        bookList.add(String.format("%-10s | %-20s | %-20s | %-10s | %-7s | %-20s | %s",
+                "ISBN", "Title", "Category", "Language", "Edition", "Publishing Year", "Author"));
 
         try {
             connection = DatabaseConnection.getConnection();
@@ -20,20 +20,20 @@ public class Display {
             resultSet = ps.executeQuery();
 
             while (resultSet.next()) {
-                int id = resultSet.getInt("ISBN");
-                String title = resultSet.getString("Title");
-                String category = resultSet.getString("Category");
-                String language = resultSet.getString("Languge");
-                String edition = resultSet.getString("Edition");
-                String publishingYear = resultSet.getString("PublisingYear");
-                String author = resultSet.getString("Name");
+                int id = resultSet.getInt("b.ISBN");
+                String title = resultSet.getString("b.Title");
+                String category = resultSet.getString("b.Category");
+                String language = resultSet.getString("b.Language");
+                String edition = resultSet.getString("b.Edition");
+                String publishingYear = resultSet.getString("b.PublishingYear");
+                String author = resultSet.getString("a.Name");
 
                 String bookInfo = String.format("%-10s | %-20s | %-20s | %-10s | %-7s | %-20s | %s",
                         id, title, category, language, edition, publishingYear, author);
                 bookList.add(bookInfo);
             }
-        } catch (Exception e) {
-            System.out.println("com.Models.user.viewAllBooks(): " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error executing SQL query: " + e.getMessage());
         } finally {
             try {
                 if (resultSet != null) {
@@ -45,7 +45,7 @@ public class Display {
                 if (connection != null) {
                     connection.close();
                 }
-            } catch (Exception e) {
+            } catch (SQLException e) {
                 System.out.println("Error closing database resources: " + e.getMessage());
             }
         }
