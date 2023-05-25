022f98981d7f7a4c96a666bb347b5f16beeb45c0
Aya Ali <101468593+AyaA1i@users.noreply.github.com>
2023-05-25 15:14:06 +0300

    Delete 20210492_20210083_20210540_20210155_20210495 directory

--- 20210492_20210083_20210540_20210155_20210495/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] args) throws SQLException {
-        Homepage runProgram = new Homepage();
-        runProgram.setVisible(true);
-    }
-}
